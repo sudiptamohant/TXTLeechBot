6f67d7020f1dc9292581bc113cc8b898f9424ee8
sudiptamohant <78973261+sudiptamohant@users.noreply.github.com>
2023-05-04 09:40:49 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
-api_id = 2363700
-api_hash = "527cf5174eigfkggigig3611bc69d7b7709"
-bot_token = "6180767554gjggkgkgifBsrDmKItUewgdKGAtGo"
+api_id = 5806640
+api_hash = "127f130ad3745dbcd31aa39aa0eabcb8"
+bot_token = "6102078578:AAHHOgYpKduh_OFRtzh5yLBOKa1KrDVqXg0"
 
